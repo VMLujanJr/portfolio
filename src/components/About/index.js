52709b386e3d622f82a55ff0bc63e8318aadb4cd
VMLujanJr <vmlujanjr@outlook.com>
2022-09-12 16:23:51 -0500

    about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 function About() {
     return (
-        <section>
-            <h1 id='about' className='text-c-blue text-7xl'>About Section</h1>
-            
+        <section id='about'>
+            <h1 className='font-Aboreto text-[72px]'>Victor Lujan</h1>
+            <p class="font-Nanum text-[20px]">
+                As a self-motivated software developer, I am searching for a reliable
+                company that can provide a safe learning environment with the potential to establish my career
+                in
+                software development.
+            </p>
+            <p class="font-Aboreto text-[20px] font-semibold">
+                available for hire
+            </p>
         </section>
     );
 };
