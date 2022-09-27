f19cdbd9be9a68ffc4b0df5fd55ca4dcde9ab357
VMLujanJr <vmlujanjr@outlook.com>
2022-09-27 17:49:28 -0500

    resume fix in progress

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
 const Resume = ({ onClose }) => {
     return (
-        <section id='resume' className='z-50 min-h-screen flex justify-center items-center bg-c-tblack absolute w-full font-League'>
+        <section id='resume' className='z-50 min-h-screen flex justify-center items-center bg-c-tblack w-full font-League fixed top-0 right-0 bottom-0 left-0'>
             <div id='resumeContainer' className='bg-c-blue px-5 flex flex-col justify-center items-center max-w-screen-lg'>
                 <button
                     type='button'
@@ -15,7 +15,6 @@ const Resume = ({ onClose }) => {
                     className='font-bold text-2xl text-c-gray flex justify-end my-5 w-full hover:text-c-black active:text-c-red'>
                     X
                 </button>
-                <h3 id='title' className='text-7xl flex justify-center'>Resume</h3>
                 <Document file={ ResumePDF } className='w-full'>
                     <Page pageNumber={1} />
                 </Document>
