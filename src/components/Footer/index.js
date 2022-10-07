d70a374b09a147c6ab3931d0958ee9b4414e2dcf
VMLujanJr <vmlujanjr@outlook.com>
2022-10-07 15:56:20 -0500

    changed icon size to 30px

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,17 +9,17 @@ const Footer = () => {
             <ul className='flex flex-col items-start space-y-5 p-2'>
                 <li>
                     <a href='mailto:vmlujanjr@outlook.com' target='_blank'>
-                        <img src={ EmailLogo } alt='email logo' className='hover:scale-110' style={{ width: '54px', height: '54px' }} />
+                        <img src={ EmailLogo } alt='email logo' className='hover:scale-110 w-[30px] h-[30px]' />
                     </a>
                 </li>
                 <li>
                     <a href='https://github.com/VMLujanJr' target='_blank'>
-                        <img src={ GitHubLogo } alt='github logo' className='hover:scale-110' style={{ width: '54px', height: '54px' }} />
+                        <img src={ GitHubLogo } alt='github logo' className='hover:scale-110 w-[30px] h-[30px]' />
                     </a>
                 </li>
                 <li>
                     <a href='https://www.linkedin.com/in/vmlujanjr/' target='_blank'>
-                        <img src={ LinkedInLogo } alt='linkedin logo' className='hover:scale-110' style={{ width: '54px', height: '54px' }} />
+                        <img src={ LinkedInLogo } alt='linkedin logo' className='hover:scale-110 w-[30px] h-[30px]' />
                     </a>
                 </li>
             </ul>
