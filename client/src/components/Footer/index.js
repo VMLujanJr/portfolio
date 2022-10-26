ee93ac428e871f4a0a6ba47dc96cff3868ba9da1
VMLujanJr <vmlujanjr@outlook.com>
2022-10-26 15:00:28 -0500

    refactored footer and logos

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import EmailLogo from '../../assets/images/logos/email.svg';
-import GitHubLogo from '../../assets/images/logos/github.svg';
-import LinkedInLogo from '../../assets/images/logos/linkedin.svg';
+import { contactInfo } from '../../utils/contactInfo.js';
 
 const Footer = () => {
     return (
-        <footer className='sticky bottom-0 z-20 bg-c-yellow'>
+        <footer className='sticky bottom-0 z-20'>
             <ul className='flex flex-col items-start space-y-5 p-2'>
-                <li>
-                    <a href='mailto:vmlujanjr@outlook.com' target='_blank' rel='noreferrer'>
-                        <img src={ EmailLogo } alt='email logo' className='hover:scale-110 w-[35px] h-[35px] tablet:w-[45px] tablet:h-[45px] laptop:w-[55px] laptop:h-[55px] desktop:w-[65px] desktop:h-[65px]' />
-                    </a>
-                </li>
-                <li>
-                    <a href='https://github.com/VMLujanJr' target='_blank' rel='noreferrer'>
-                        <img src={ GitHubLogo } alt='github logo' className='hover:scale-110 w-[35px] h-[35px]' />
-                    </a>
-                </li>
-                <li>
-                    <a href='https://www.linkedin.com/in/vmlujanjr/' target='_blank' rel='noreferrer'>
-                        <img src={ LinkedInLogo } alt='linkedin logo' className='hover:scale-110 w-[35px] h-[35px]' />
-                    </a>
-                </li>
+                {contactInfo.map((info, index) => (
+                    <li key={index}>
+                        <a href={info.href} target={info.target} rel={info.rel}>
+                            <img src={info.src} alt={info.alt} className='hover:scale-110 w-[35px] h-[35px] tablet:w-[45px] tablet:h-[45px] laptop:w-[55px] laptop:h-[55px] desktop:w-[65px] desktop:h-[65px]' />
+                        </a>
+                    </li>
+                ))}
             </ul>
         </footer>
     );
--- client/src/utils/contactInfo.js
+++ client/src/utils/contactInfo.js
@@ -1,27 +1,27 @@
-import EmailLogo from '../assets/images/logos/email.svg';
-import GitHubLogo from '../assets/images/logos/github.svg';
-import LinkedInLogo from '../assets/images/logos/linkedin.svg';
+import EmailLogo from '../assets/logos/email.svg';
+import GitHubLogo from '../assets/logos/github.svg';
+import LinkedInLogo from '../assets/logos/linkedin.svg';
 
 export const contactInfo = [
     {
         href: 'mailto:vmlujanjr@outlook.com' ,
         target: '_blank',
         rel: 'noreferrer',
-        src: <EmailLogo />,
-        alt: 'email logo'
+        src: EmailLogo,
+        alt: 'email'
     },
     {
         href: 'https://github.com/VMLujanJr' ,
         target: '_blank',
         rel: 'noreferrer',
-        src: <GitHubLogo />,
-        alt: 'github logo'
+        src: GitHubLogo,
+        alt: 'github'
     },
     {
         href: 'https://www.linkedin.com/in/vmlujanjr/' ,
         target: '_blank',
         rel: 'noreferrer',
-        src: <LinkedInLogo />,
-        alt: 'linkedin logo'
+        src: LinkedInLogo,
+        alt: 'linkedin'
     },
 ];
