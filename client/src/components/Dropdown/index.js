d135413ff3835cdf7bb40fe79315b1615339353b
VMLujanJr <vmlujanjr@outlook.com>
2022-10-28 15:23:38 -0500

    fixed navigation issues

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -10,7 +10,7 @@ const Dropdown = ({ submenus, dropdown }) => {
                         to={submenu.url}
                         target={submenu.target}
                         rel={submenu.relation}
-                        className='hover:text-c-blue block px-4 py-2 text-xl animate-nav3-down laptop:animate-nav3-slide active:text-c-yellow'
+                        className='text-c-white hover:text-c-blue block px-4 py-2 text-xl animate-nav3-down laptop:animate-nav3-slide active:text-c-yellow'
                     >
                         {submenu.title}
                     </Link>
--- client/src/components/MenuItems/index.js
+++ client/src/components/MenuItems/index.js
@@ -2,20 +2,37 @@ import React, { useState, useEffect, useRef } from 'react';
 import Dropdown from '../Dropdown';
 
 const MenuItems = ({ items }) => {
-    const [dropdown, setDropdown] = useState(null);
     let ref = useRef(); // access the DOM elements of the dropdown by passing a reference to the target node
+    
+    const [dropdown, setDropdown] = useState(Boolean); // Toggle Dropdown ON / OFF
+    const [dimensions, setDimensions] = useState({
+        height: window.innerHeight,
+        width: window.innerWidth
+    });
+
+/*     useEffect(() => {
+        if (dimensions.width >= 1024) {
+            setDropdown(true);
+        } else if (dimensions.width < 1024) {
+            setDropdown(false);
+        }
+    }); */
+
+    /* const [onPageReload, setOnPageReload] = useState(Boolean); */
 
     /* if you click outside of the menu, the menu items will collapse */
     useEffect(() => {
         const handler = (event) => {
+
             /* I don't understand !ref.current.contains(event.target) */
-            if (dropdown && ref.current && !ref.current.contains(event.target)) {
-                setDropdown(false);
+            if (dimensions.width < 1024 && dropdown && ref.current && !ref.current.contains(event.target)) { // this fixed the click issue
+                setDropdown(false); // this is what is turning it off
             }
         };
 
         document.addEventListener('mousedown', handler);
         document.addEventListener('touchstart', handler);
+        /* document.addEventListener('load', onPageReload); */ // we should create a handler;
 
         return () => {
             // cleanup the event listener
@@ -23,11 +40,6 @@ const MenuItems = ({ items }) => {
             document.removeEventListener('touchstart', handler);
         };
     }, [dropdown]); // ...true or false...
-
-    const [dimensions, setDimensions] = useState({
-        height: window.innerHeight,
-        width: window.innerWeight
-    });
 
     useEffect(() => {
         const handleResize = () => {
@@ -38,38 +50,41 @@ const MenuItems = ({ items }) => {
         }
 
         window.addEventListener('resize', handleResize);
-        
+
         return () => {
             window.removeEventListener('resize', handleResize);
-        }
+        };
     });
 
     useEffect(() => {
-            if (dimensions.width >= 1024) {
-                setDropdown(true);
-            } else if (dimensions.width < 1024) {
-                setDropdown(false);
-            }
+        if (dimensions.width >= 1024) {
+            setDropdown(true);
+        } else if (dimensions.width < 1024) {
+            setDropdown(false);
+        }
     }, [dimensions]);
 
-    /* useEffect(() => {
-        const handleResize = () => {
-            if (window.innerWidth >= 1024 && !dropdown) {
-                setDropdown(true);
-            } else if (window.innerWidth < 1024 && dropdown) {
-                setDropdown(false);
-            }
-            console.log(`resized to: `, window.innerWidth, `x`, window.innerHeight);
+/*     useEffect(() => {
+        if (dimensions.width >= 1024) {
+            setDropdown(true);
         }
-        window.addEventListener('resize', handleResize);
+    }, [onPageReload]);
+
+    useEffect(() => {
+        const handlePageReload = () => {
+            setOnPageReload(true);
+            console.log('reloaded');
+        };
+
+        window.addEventListeneer('load', handlePageReload);
 
         return () => {
-            window.removeEventListener('resize', handleResize);
-        }
+            window.removeEventListener('load', handlePageReload);
+        };
     }); */
 
     const onMouseEnter = () => {
-        window.innerWidth < 1024 && setDropdown(true); 
+        window.innerWidth < 1024 && setDropdown(true);
     };
 
     const onMouseLeave = () => {
